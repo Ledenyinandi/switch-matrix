2182141a9d280ab24ebf69d4f36cf2e08456edcb
Ledenyinandi <ledenyinandi@gmail.com>
2021-11-09 17:33:34 +0100

    Add toggle method

--- src/main/java/nandor/ledenyi/switchmatrix/SwitchMatrix.java
+++ src/main/java/nandor/ledenyi/switchmatrix/SwitchMatrix.java
@@ -37,4 +37,33 @@ public class SwitchMatrix {
             }
         }
     }
+
+    public void toggle(int x, int y) {
+        if (board[x][y]) {
+            board[x][y] = !board[x][y];
+            if (x < board.length - 1) {
+                board[x + 1][y] = !board[x + 1][y];
+            }
+            if (x > 0) {
+                board[x - 1][y] = !board[x - 1][y];
+            }
+            if (y < board.length - 1) {
+                board[x][y + 1] = !board[x][y + 1];
+            }
+            if (y > 0) {
+                board[x][y - 1] = !board[x][y - 1];
+            }
+        }
+    }
+
+    public boolean isSolved() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
